51de2e464c9c1068bbbaf2f4eac61617c77b52d9
Antony Stubbs <antony.stubbs@gmail.com>
2017-01-19 15:54:05 -0500

    Fix syntax

--- k-read-all-avro.fish
+++ k-read-all-avro.fish
@@ -6,16 +6,3 @@ function k-read-all-avro
           --property print.key=true \
           --property key.deserializer=org.apache.kafka.common.serialization.StringDeserializer 
 end
-
-	./bin/kafka-avro-console-consumer \
-	          --topic incoming-messages-2 \
-         	  --zookeeper localhost:2181 \
-         	  --from-beginning \
-          --property print.key=true 
-
-
-./bin/kafka-avro-console-consumer --topic incoming-messages-2 \
-         --zookeeper localhost:2181 \
-         --from-beginning
-
-         
